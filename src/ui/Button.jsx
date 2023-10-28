ace169560058c920d38e3bf8aed4b070ba2ab297
Djumash <djumash.violin@gmail.com>
2023-10-28 13:14:57 +0100

    Updating styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,4 @@
 import styled, { css } from "styled-components";
-
 const sizes = {
   small: css`
     font-size: 1.2rem;
@@ -49,19 +48,16 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.5 rem;
-  padding: 1.2 rem;
-  font-weight: 500;
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
   box-shadow: var(--shadow-sm);
-  cursor: pointer;
 
-  &:hover {
-    background-color: var(--color-brand-700);
-  }
+  ${(props) => sizes[props.size]};
+  ${(props) => variations[props.variation]}
 `;
 
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 export default Button;
