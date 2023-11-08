58ac575ad6f8b7571da4a284e86b17cb7af51c8b
Djumash <djumash.violin@gmail.com>
2023-11-08 18:47:18 +0000

    Adding Pie Chart and Graph using Reacharts library to the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -32,8 +34,8 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>List of activity</div>
-      <div>Stay durations</div>
-      <div>Chart of sales</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
